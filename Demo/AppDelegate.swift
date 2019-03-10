b9f13aed02dc11387714e5aae0e74c35c5a87aa9
Shiv <shivshankar938@gmail.com>
2019-03-10 21:04:22 +0530

    Newcommit

--- Demo/AppDelegate.swift
+++ Demo/AppDelegate.swift
@@ -19,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         IQKeyboardManager.shared.enable = true
         UINavigationBar.appearance().barStyle = .blackOpaque
+        UINavigationBar.appearance().barStyle = .blackOpaque
 
 //
         return true
